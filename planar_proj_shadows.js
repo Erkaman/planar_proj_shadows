bb21ada4576ea69e7817d2ac484baa58bed3297a
Eric Arnebäck <arnebackeric@gmail.com>
2017-03-11 21:12:13 +0100

    Add github banner.

--- planar_proj_shadows.js
+++ planar_proj_shadows.js
@@ -1,10 +1,16 @@
-const webglCanvas = document.body.appendChild(document.createElement('canvas'))
+const canvas = document.body.appendChild(document.createElement('canvas'))
+var str = `<a href="https://github.com/Erkaman/planar_proj_shadows"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/82b228a3648bf44fc1163ef44c62fcc60081495e/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png"></a>`
+
+var container = document.createElement('div')
+container.innerHTML = str
+document.body.appendChild(container)
+
 const fit = require('canvas-fit')
 const regl = require('regl')({
-  canvas: webglCanvas})
+  canvas: canvas})
 const mat4 = require('gl-mat4')
 const vec3 = require('gl-vec3')
-window.addEventListener('resize', fit(webglCanvas), false)
+window.addEventListener('resize', fit(canvas), false)
 const bunny = require('bunny')
 const normals = require('angle-normals')
 
